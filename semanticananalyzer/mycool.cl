1713d343fd9343c5f7e7b1c2352129215279668e
John Wheatley Lambert <johnwl@myth63.stanford.edu>
2018-06-07 22:50:19 -0700

    add

---
+++ semanticananalyzer/mycool.cl
@@ -0,0 +1,7 @@
+class Main inherits IO {
+    main() : SELF_TYPE {
+	{
+	    out_string("\n");
+	}
+    };
+};
--- semanticananalyzer/test_semant.sh
+++ semanticananalyzer/test_semant.sh
@@ -1,5 +1,9 @@
 
-for f in /afs/ir.stanford.edu/class/cs143/examples/*.cl; do
+#for f in /afs/ir.stanford.edu/class/cs143/examples/*.cl; do
+#for f in examples/primes.cl; do
+#for f in examples/atoi.cl; do
+#for f in examples/cool.cl; do
+for f in mycool.cl; do
         echo "File -> $f"
         ./mysemant -s $f > my_output.txt
         ./theirsemant $f > ref_output.txt
